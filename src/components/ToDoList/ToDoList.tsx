5ddad7e3bf1889593aca9f00f0cda1bc3d5f9342
alfi1210 <alfred.vizintin@gmail.com>
2024-11-01 23:37:15 +0100

    To Do list

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -2,11 +2,45 @@ import { useState } from "react";
 import styles from "./ToDoList.module.css";
 
 export default function ToDoList() {
-  const [tasks, setTasks] = useState(["Task 1", "Task 2", "Task 3"]);
+  const [tasks, setTasks] = useState<string[]>([]);
   const [newTask, setNewTask] = useState("");
 
   function imputChange(e: any) {
     setNewTask(e.target.value);
+  }
+
+  function addNewTask() {
+    if (newTask.trim() !== "") {
+      setTasks([...tasks, newTask]);
+      setNewTask("");
+    }
+  }
+
+  function deleteTask(index: number) {
+    const updatedTasks = tasks.filter((_, i: number) => i !== index);
+    setTasks(updatedTasks);
+  }
+
+  function moveTaskUp(index: number) {
+    if (index > 0) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index - 1]] = [
+        updatedTasks[index - 1],
+        updatedTasks[index],
+      ];
+      setTasks(updatedTasks);
+    }
+  }
+
+  function moveTaskDown(index: number) {
+    if (index !== tasks.length - 1) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index + 1]] = [
+        updatedTasks[index + 1],
+        updatedTasks[index],
+      ];
+      setTasks(updatedTasks);
+    }
   }
 
   return (
@@ -20,16 +54,31 @@ export default function ToDoList() {
         value={newTask}
         onChange={imputChange}
       />
-      <button>
+      <button className={styles.addButton} onClick={addNewTask}>
         <i className="fa-solid fa-plus fa-xs"></i>
       </button>
       <div>
         <ul>
           {tasks.map((task, index) => (
             <li key={index} className={styles.task}>
-              {task}
-              <button>
+              <span className={styles.taskText}>{task}</span>
+              <button
+                className={styles.deleteButton}
+                onClick={() => deleteTask(index)}
+              >
                 <i className="fas fa-trash-can fa-xs"></i>
+              </button>
+              <button
+                className={styles.moveButton}
+                onClick={() => moveTaskUp(index)}
+              >
+                <i className="fas fa-arrow-up fa-xs"></i>
+              </button>
+              <button
+                className={styles.moveButton}
+                onClick={() => moveTaskDown(index)}
+              >
+                <i className="fas fa-arrow-down fa-xs"></i>
               </button>
             </li>
           ))}
@@ -37,4 +86,7 @@ export default function ToDoList() {
       </div>
     </div>
   );
+}
+function _(value: string, index: number, array: string[]): value is string {
+  throw new Error("Function not implemented.");
 }
